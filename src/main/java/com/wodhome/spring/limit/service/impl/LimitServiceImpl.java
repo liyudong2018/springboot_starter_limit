4fae9c0b089bf590c18af1ee8ce52e9271b95b3d
liyudong1818 <yudongli756@gmail.com>
2023-06-13 23:30:22 +0800

    Changes

--- src/main/java/com/wodhome/spring/limit/service/impl/LimitServiceImpl.java
+++ src/main/java/com/wodhome/spring/limit/service/impl/LimitServiceImpl.java
@@ -3,6 +3,7 @@ package com.wodhome.spring.limit.service.impl;
 import com.wodhome.spring.limit.LimitConfig;
 import com.wodhome.spring.limit.model.BaseData;
 import com.wodhome.spring.limit.service.LimitService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Description: java类作用描述
@@ -10,15 +11,27 @@ import com.wodhome.spring.limit.service.LimitService;
  * @CreateDate: 2023/6/12
  * @Version: 1.0
  */
-
+@Slf4j
 public class LimitServiceImpl implements LimitService {
     @Override
     public void addData(String name, BaseData data) {
-        LimitConfig.rateModelConcurrentHashMap.get(name).addData(data);
+        if(LimitConfig.rateModelConcurrentHashMap.get(name)!=null){
+            LimitConfig.rateModelConcurrentHashMap.get(name).addData(data);
+        }else{
+            log.error("不存在name={}的队列",name);
+        }
+
     }
 
     @Override
     public BaseData getData(String name) {
-        return  LimitConfig.rateModelConcurrentHashMap.get(name).getLimitData();
+
+        if(LimitConfig.rateModelConcurrentHashMap.get(name)!=null){
+            return  LimitConfig.rateModelConcurrentHashMap.get(name).getLimitData();
+        }else{
+            log.error("不存在name={}的队列",name);
+            return null;
+        }
+
     }
 }
